3cf9469c60893415033bccd0a8586365e2dbcd79
Rock-it-science <wilmc17@gmail.com>
2018-12-09 23:44:50 -0800

    Complete wheel-size compatibility checking bw fork and frame

--- js/compatibility/wheelSize.js
+++ js/compatibility/wheelSize.js
@@ -1,4 +1,8 @@
 async function wheelSizeCompat(){
+
+  //Gets wheelsize selections for frame and fork from server using async
+  // AJAX calls, then calls back to check if they are compatible or not.
+
   var frameWS;
   var forkWS;
 
@@ -8,9 +12,18 @@ async function wheelSizeCompat(){
   }).then(getForkOptions(function(b){
     console.log(b);
     forkWS = b;
+    console.log("later "+ frameWS);
+    if(frameWS == forkWS){
+      console.log("compatible");
+      document.getElementById("frameCompat").innerHTML = "Compatible with fork";
+      document.getElementById("forkCompat").innerHTML = "Compatible with frame";
+    }
+    else{
+      console.log("not compatible");
+      document.getElementById("frameCompat").innerHTML = "NOT compatible with fork";
+      document.getElementById("forkCompat").innerHTML = "NOT compatible with frame";
+    }
   }));
-
-    //Figure out how to return if forkWS == frameWS to index
 
   //Get wheelsize from frame and fork options
   async function getFrameOptions(callback){
@@ -20,7 +33,7 @@ async function wheelSizeCompat(){
         //console.log("frameRequest status = " + isframeRequest.status);
         if(frameRequest.readyState == 4 && frameRequest.status == 200){
           var ws = frameRequest.responseText.split(" ")[2];
-          console.log(ws);
+          //console.log(ws);
           resolve(ws)
         }
       };
@@ -38,7 +51,7 @@ async function wheelSizeCompat(){
         //console.log("forkRequest status = " + isforkRequest.status);
         if(forkRequest.readyState == 4 && forkRequest.status == 200){
           var ws = forkRequest.responseText.replace(' ','');
-          console.log(ws);
+          //console.log(ws);
           resolve(ws);
         }
       };
